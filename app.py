d4c4bccdc2f505bcbba7c71e623b0bd1f5b9e3fd
CatalinaFonsecaM <140755001+CatalinaFonsecaM@users.noreply.github.com>
2023-10-19 14:19:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import geopandas as gdp
 st.set_page_config(layout='wide')
 st.title("Medición del bienestar")
 df = pd.read_csv('dataframe.csv')
-df2 = pd.read_csv('dataframe2.csv')
+df2 = pd.read_geojson('dataframe2.geojson')
 
 tab1, tab2, tab3, tab4 = st.tabs(['Análisis estadístico', 'Análisis gráfico específico', 'Análisis georreferenciado', 'Indicador de bienestar'])
 
