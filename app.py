d6cbc4877930a624916bb41251eeb8275ea2416e
CatalinaFonsecaM <140755001+CatalinaFonsecaM@users.noreply.github.com>
2023-10-19 14:21:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
-import geopandas as gdp
+import geopandas as gpd
 
 
 st.set_page_config(layout='wide')
 st.title("Medición del bienestar")
 df = pd.read_csv('dataframe.csv')
-df2 = pd.read_geojson('dataframe2.geojson')
+df2 = gpd.read_geojson('dataframe2.geojson')
 
 tab1, tab2, tab3, tab4 = st.tabs(['Análisis estadístico', 'Análisis gráfico específico', 'Análisis georreferenciado', 'Indicador de bienestar'])
 
