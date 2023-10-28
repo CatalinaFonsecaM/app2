90ebf0d318702975b107d42f68862864d8bb5087
CatalinaFonsecaM <140755001+CatalinaFonsecaM@users.noreply.github.com>
2023-10-28 08:52:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(layout='wide')
 st.title("Medición del bienestar")
 df = pd.read_csv('dataframe.csv')
 df2 = gpd.read_file('dataframe2.geojson')
-df3 = gpd.read_file('df4.geojson')
+df3 = gpd.read_file('df3.geojson')
 
 
 tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['Análisis estadístico', 'Análisis gráfico específico', 'Análisis de hipótesis', 'Análisis georreferenciado', 'Indicador de bienestar', 'Creado por:'])
@@ -796,7 +796,7 @@ with tab5:
         df_year = df3[(df3['año'] >= start_year) & (df3['año'] <= end_year)]
     
         df_year.plot(
-            column="indicadorf_sqrt",
+            column="indicador",
             scheme="Quantiles",
             cmap="plasma",
             legend=True,
